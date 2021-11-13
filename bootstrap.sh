0522f699f1a57fa52eafb6f86131113e1eb78bb4
Lars <lars@lmb.local>
2021-11-13 18:45:30 +0100

    operating system check added

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# bootsrapping files for system setup
+
+# systemcheck
+clear
+
+UNAME="$(uname)"
+
+case $UNAME in
+  'Darwin')
+    OSNAME='MacOS'
+    ;;
+  'Linux')
+    OSNAME='Linux'
+    ;;
+  *)
+    ;;
+esac
