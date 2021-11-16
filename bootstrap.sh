9d2e5eee7422b1c1bbe502185c172c61ca3441f7
Lars <lars@lmb.local>
2021-11-16 16:02:57 +0100

    test

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env zsh
 
-# bootsrapping files for system setup
-
 clear
-echo "Bootstrap installation files\n"
+
+echo "Systemcheck"
+echo "-----------"
 
 OSTYPE="$(uname -s)"
 
 case $OSTYPE in
   'Darwin')
     OS='macOS'
-    CPUTYPE="$(uname -p)"
-    if [[ "$CPUTYPE" == "arm64" ]]
-    then
-      CPU="Apple"
-    else
-      CPU="Intel"
-    fi
     ;;
   'Linux')
     OS='linux'
@@ -25,9 +18,26 @@ case $OSTYPE in
     ;;
 esac
 
-echo "Systemcheck\n"
-echo "-----------\n"
-echo "OS:  $OS"
-echo "CPU: $CPU"
+echo "OS:\t${OS}"
 
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/keepkode/install/main/setup/$OS.sh)"
+ZSHMSG="ZSH:\t"
+if [[ !  $ZSH_VERSION -ge 5.3 ]]; then
+  ${ZSHMSG}+=
+  echo "${ZSHMSG}not ok, Version => 5.3 required\n\tQuit!"
+  exit 1 
+else
+  echo "${ZSHMSG}ok"
+fi
+
+PYMSG="Python:\t"
+if [ !  /usr/bin/python ]; then
+  echo "${PYMSG}not found.\n\tQuit!"
+  exit 1
+else 
+  echo "${PYMSG}ok"
+fi
+
+echo "\nStarting Setup"
+echo "--------------\n"
+echo "- downloading macOS Setupfiles"
+
--- setup/macOS.sh
+++ setup/macOS.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env zsh
 
-if [[ $ZSH_VERSION -ge 5.3 ]]; then
-  echo "zsh version ok"
-else
-  echo "ZSH Version too old\nquit"
-  exit 1 
-fi
-
-
 OS=$(uname -s)
 CPU=$(uname -p)
 
-# Setup variables
+# install CommandLineTools
+
+COMMANDLINETOOLS="/Library/Developer/CommandLineTools"
+if [ ! -d $COMMANDLINETOOLS ]; then
+  echo "installing Commandline Tools"
+  xcode-select --install
+else
+  echo "Commandline Tools installed"
+fi
+
 # HOMEBREW
 if [[ "$CPU" == "arm64" ]]
 then 
@@ -20,53 +21,9 @@ else
   HOMEBREW="/usr/local/bin/brew"
 fi
 
-# Commandlinetools
-COMMANDLINETOOLS="/Library/Developer/CommandLineTools"
-ZSHCONFIGDIR="$HOME/.config/zsh"
-
-installstack=()
-
-echo "----------"
-echo "Checking prequisites"
-echo "----------\n"
-
-# prequisites
-# add additional prequisites as "<name>" "<type>"
-declare -A prequisites
-prequisites=(
-  "HOMEBREW" "file"
-  "ZDOTDIR" "variable"
-  "COMMANDLINETOOLS" "directory"
-  "XZSH_VERSION" "variable"
-
-)
-
-# sed -i '' -e '1s/^/# Setup ZDOTDIR\nZDOTDIR=$HOME\/\.config\/zsh\n/'
-
-for key value in ${(kv)prequisites}; do
-  case $value in
-    'variable')
-      TEST='-v'
-      ;;
-    'file')
-      TEST="-f"
-        ;;
-    'directory')
-      TEST="-d"
-      ;;
-    'xxx')
-      TEST="-d"
-      ;;
-    *)
-      ;;
-  esac
-  if [ ! "$TEST" $key ]; then
-      installstack+=("setup_$key")
-  fi
-done
-
-for i in $installstack
-do
-  echo "$i"
-done
+if [ ! -f $HOMEBREW ];then
+  echo "installing Homebrew"
+else 
+  echo "Homebrew installed"
+fi
 
